4b0cada5c9c4f7b5408889b74e30f4f0528bb562
DJ-archive <7djyun7@gmail.com>
2022-07-13 22:25:04 +0900

    feat: simple restcontroller

---
+++ src/main/java/com/djyun/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.djyun.restfulwebservice;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloWorldController {
+    // GET
+    // /hello-word (endpoint)
+    //@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "helloWorld";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("hello world");
+    }
+
+    @GetMapping("/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World, %s",name));
+    }
+}
