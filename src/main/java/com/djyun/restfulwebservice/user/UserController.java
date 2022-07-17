b092fc8509c6844ab7f0aa6f25a85b7f158f2e75
DJ-archive <7djyun7@gmail.com>
2022-07-17 13:37:25 +0900

    feat: GET User API

---
+++ src/main/java/com/djyun/restfulwebservice/user/UserController.java
@@ -0,0 +1,28 @@
+package com.djyun.restfulwebservice.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    private UserDaoService service;
+
+    public UserController(UserDaoService service) { // 생성자 주입
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return service.findAll();
+    }
+
+    // GET /users/1 or /users/10 -> String
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id){ // 자동으로 int converting
+        return service.findOne(id);
+    }
+
+}
